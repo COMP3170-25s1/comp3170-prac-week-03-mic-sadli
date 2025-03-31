17fe41381a67944f76dd60725428ffd3d218f894
Michael Sadli <126532262+mic-sadli@users.noreply.github.com>
2025-03-31 16:24:34 +1100

    yipeee

--- src/comp3170/week3/vertex.glsl
+++ src/comp3170/week3/vertex.glsl
@@ -1,6 +1,9 @@
 #version 410
 
 in vec4 a_position;	// vertex position as a homogenous vector in NDC 
+
+uniform mat4 u_modelMatrix;
+
 in vec3 a_colour; // vertex colour RGB
 
 out vec3 v_colour; // to fragment shader
@@ -8,8 +11,9 @@ out vec3 v_colour; // to fragment shader
 void main() {
 	v_colour = a_colour;
 
+
 	// pad the vertex to a homogeneous 3D point
-    gl_Position = a_position;
+    gl_Position = u_matrix * a_position;
 
 }
 
